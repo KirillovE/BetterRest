4fad87e6f769f137a4a235815e5383273db49784
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-14 08:05:31 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Евгений Кириллов on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
