0054aec69c6df295f408374561fc044075faace5
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-16 08:41:59 +0300

    Changing UI during chalenge

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,17 +11,12 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeUpTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
+    @State private var recomendation = "Your recomendation"
     
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: nil) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section(header: Text("When do you want to wake up?")) {
                     DatePicker(
                         "Please enter a time",
                         selection: $wakeUp,
@@ -30,9 +25,7 @@ struct ContentView: View {
                     .labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: nil) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                Section(header: Text("Desired amount of sleep")) {
                     Stepper(
                         value: $sleepAmount,
                         in: 4...12,
@@ -42,30 +35,40 @@ struct ContentView: View {
                     }
                 }
                 
-                VStack(alignment: .leading, spacing: nil) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    Stepper(value: $coffeeAmount, in: 1...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
+                Section(header: Text("Daily coffee intake")) {
+                    Picker(
+                        selection: $coffeeAmount,
+                        label: Text("1 cup")
+                    ) {
+                        ForEach(1 ..< 21) {
+                            if $0 == 1 {
+                                Text("1 cup")
+                            } else {
+                                Text("\($0) cups")
+                            }
+
                         }
                     }
+                    .pickerStyle(InlinePickerStyle())
+                }
+                
+                Section(header: Text("Recommended bedtime")) {
+                    Text(recomendation)
+                        .font(.title)
                 }
             }
             .navigationBarTitle("Better rest")
-            .navigationBarItems(
-                trailing: Button(action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
-            .alert(isPresented: $showingAlert) {
-                Alert(
-                    title: Text(alertTitle),
-                    message: Text(alertMessage),
-                    dismissButton: .default(Text("OK"))
-                )
+            .onAppear {
+                calculateBedtime()
+            }
+            .onChange(of: coffeeAmount) { _ in
+                calculateBedtime()
+            }
+            .onChange(of: sleepAmount) { _ in
+                calculateBedtime()
+            }
+            .onChange(of: wakeUp) { _ in
+                calculateBedtime()
             }
         }
     }
@@ -94,13 +97,10 @@ struct ContentView: View {
             
             let formatter = DateFormatter()
             formatter.timeStyle = .short
-            alertMessage = formatter.string(from: sleepTime)
-            alertTitle = "Your ideal bedtime is…"
+            recomendation = formatter.string(from: sleepTime)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            recomendation = "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
 }
 
